dc8dc0563b43c6af02fe55de262d12a423f93d90
Egasyahputri <egasyaputri2005@gmail.com>
2024-11-14 21:49:07 +0700

    Add files via upload

---
+++ registrasi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Form Registrasi</h1>
+    <form method="post" action="proses_registrasi.php">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" value="Registrasi">
+    </form>
+</body>
+</html>
